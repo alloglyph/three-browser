cc0c5a1309c0c3757d252d0642106eea60d62e34
Alan G <alloglyph@gmail.com>
2016-02-20 13:06:08 -0800

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const gulp          = require('gulp');
+const browserify    = require('browserify');
+const babelify      = require('babelify');
+const source        = require('vinyl-source-stream');
+const chalk         = require('chalk');
+
+const ENTRY_FILE    = './index.js';
+
+function error(s) {
+  console.log(chalk.bold.red(s));
+}
+
+function info(s) {
+  console.log(chalk.bold.green(s));
+}
+
+gulp.task('build', function () {
+  info('♺ rebuilding ...');
+  return browserify(ENTRY_FILE, { debug: true })
+          .transform(babelify)
+          .bundle()
+          .on('error', function (err) { error(err); })
+          .pipe(source('bundle.js'))
+          .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', ['build'], function () {
+  gulp.watch(ENTRY_FILE, ['build']);
+});
+
+gulp.task('default', ['watch']);
---
+++ index.js
@@ -0,0 +1,85 @@
+const THREE = require("three");
+
+const mm = 0.1;
+const cm = 1.0;
+
+let mode = "+pitch";
+
+let scene = new THREE.Scene();
+let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+let renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+let icosahedronGeometry = new THREE.IcosahedronGeometry(5);
+let icosahedronMaterial = new THREE.MeshLambertMaterial({
+  color: 0x994d00,
+  emissive: 0x000000,
+  wireframeLinewidth: 1,
+  reflectivity: 1,
+  refractionRatio: 0.98
+});
+
+let icosahedron = new THREE.Mesh(icosahedronGeometry, icosahedronMaterial);
+let rotationGroup = new THREE.Group();
+rotationGroup.add(icosahedron);
+scene.add(rotationGroup);
+
+let ambientLight = new THREE.AmbientLight(0x404040);
+let lights = [];
+lights[0] = new THREE.PointLight( 0xffffff, 1, 0 );
+lights[1] = new THREE.PointLight( 0xffffff, 1, 0 );
+lights[2] = new THREE.PointLight( 0xffffff, 1, 0 );
+
+lights[0].position.set( 0, 200, 0 );
+lights[1].position.set( 100, 200, 100 );
+lights[2].position.set( -100, -200, -100 );
+
+scene.add(lights[0])
+scene.add(lights[1])
+scene.add(lights[2])
+
+camera.position.z = 13; // the camera is 13 cm from the origin on the z axis
+
+let render = function() {
+  requestAnimationFrame(render);
+
+  switch (mode) {
+      case "+pitch":
+          rotationGroup.rotation.x += 0.01;
+          break;
+      case "-pitch":
+          rotationGroup.rotation.x -= 0.01;
+          break;
+      case "+roll":
+          rotationGroup.rotation.y += 0.01;
+          break;
+      case "-roll":
+          rotationGroup.rotation.y -= 0.01;
+          break;
+      case "+yaw":
+          rotationGroup.rotation.z += 0.01;
+          break;
+      case "-yaw":
+          rotationGroup.rotation.z -= 0.01;
+          break;
+      default:
+          break;
+  }
+  renderer.render(scene, camera);
+};
+
+render();
+
+["pitch", "yaw", "roll"].forEach((axis) => {
+    document.getElementById(`${axis}-button`).addEventListener('click', () => {
+        switch (mode) {
+            case `+${axis}`:
+                mode = `-${axis}`; break;
+            case `-${axis}`:
+                mode = ""; break;
+            default:
+                mode = `+${axis}`; break;
+        }
+    });
+});
